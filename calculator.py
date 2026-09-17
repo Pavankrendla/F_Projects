7bdccc27a602d0dd701a8e61fe1b0fe8653fc299
Pavankrendla <endlapavankumar41@gmail.com>
2026-09-17 16:54:19 +0530

    Update calculator

--- calculator.py
+++ calculator.py
@@ -8,3 +8,4 @@ print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 print("Division:", a / b)
 #code is completed with proper measures and bullets
+#when i bought the new shoes, i had 10k expenses
